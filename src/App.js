22c744e5acc350e15e35ff0c36ef24fec1497cee
Egioooo <134957932+SergDeved@users.noreply.github.com>
2023-06-06 17:31:29 +0200

    Styles & html main structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
 const App = () => {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    const surpriseOptions = [
+        'Un raton dando vueltas',
+        'Un mono comiendo pan',
+        'Un programador frustrado',
+    ]
+    return (
+        <div className="app">
+            <section className="search-section">
+                <p> Añade una descripción
+                    <span className="surprise">Sorprendeme</span>
+                </p>
+                <div className="input-container">
+                    <input className="" placeholder="Un telefono dando voleteretas"/>
+                    <button>Generar</button>
+                </div>
+            </section>
+            <section className="image-section"></section>
+        </div>
+    );
 }
 
 export default App;
