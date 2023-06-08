271a6550e53f95cf89399a37d0d86a004c6b9e13
Egioooo <134957932+SergDeved@users.noreply.github.com>
2023-06-09 01:32:47 +0200

    Dark Mode Toggle

--- src/App.js
+++ src/App.js
@@ -1,55 +1,72 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import prompts from "./prompts";
 import Modal from "./components/Modal";
 
 const App = () => {
-    const [images, setImages] = useState(null)
-    const [value, setValue] = useState("")
-    const [error, setError] = useState(null)
-    const [selectedImage, setSelectedImage] = useState(null)
-    const [modalOpen, setModalOpen] = useState(false)
+    const [images, setImages] = useState(null);
+    const [value, setValue] = useState("");
+    const [error, setError] = useState(null);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
     const [fileError, setFileError] = useState(null);
     const [generating, setGenerating] = useState(false);
+    const [darkMode, setDarkMode] = useState(false);
 
+    useEffect(() => {
+        if (
+            window.matchMedia &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+        ) {
+            setDarkMode(true);
+        } else {
+            setDarkMode(false);
+        }
+    }, []);
 
+    useEffect(() => {
+        if (darkMode) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
+    }, [darkMode]);
 
     const surpriseMe = () => {
         const randomValue = prompts[Math.floor(Math.random() * prompts.length)];
-        setValue(randomValue)
-    }
+        setValue(randomValue);
+    };
 
     const getImages = async () => {
         setImages(null);
-        if (value === null || value.trim() === '') {
-            setError('Error! Must have a search term');
+        if (value === null || value.trim() === "") {
+            setError("Error! Must have a search term");
             return;
         } else {
             setError(null);
         }
 
         try {
-            setGenerating(true); // Habilitar "generating" antes de hacer la solicitud
+            setGenerating(true);
 
             const options = {
-                method: 'POST',
+                method: "POST",
                 body: JSON.stringify({
-                    message: value
+                    message: value,
                 }),
                 headers: {
-                    'Content-Type': 'application/json'
-                }
+                    "Content-Type": "application/json",
+                },
             };
 
-            const response = await fetch('http://192.168.0.100:8000/images', options);
+            const response = await fetch("http://192.168.0.100:8000/images", options);
             const data = await response.json();
             setImages(data);
         } catch (error) {
             console.error(error);
         } finally {
-            setGenerating(false); // Deshabilitar "generating" después de la solicitud (éxito o error)
+            setGenerating(false);
         }
     };
-
 
     const uploadImage = async (e) => {
         const file = e.target.files[0];
@@ -58,84 +75,92 @@ const App = () => {
             return;
         }
 
-        // Verificar la extensión del archivo
         if (file.type !== "image/png") {
             setFileError("Only PNG files are allowed");
             return;
         }
 
-        const formData = new FormData()
-        setModalOpen(true)
-        formData.append('file', e.target.files[0])
-        setSelectedImage(e.target.files[0])
-        e.target.value = null
+        const formData = new FormData();
+        setModalOpen(true);
+        formData.append("file", e.target.files[0]);
+        setSelectedImage(e.target.files[0]);
+        e.target.value = null;
 
         try {
             const options = {
                 method: "POST",
-                body: formData
-            }
-            const response = await fetch('http://192.168.0.100:8000/upload', options)
-            const data = await response.json()
-
+                body: formData,
+            };
+            const response = await fetch("http://192.168.0.100:8000/upload", options);
+            const data = await response.json();
         } catch (error) {
-            console.error(error)
+            console.error(error);
         }
-    }
+    };
 
     const generateVariations = async () => {
-        setImages(null)
+        setImages(null);
         if (selectedImage === null) {
-            setError('Error! Must have an existing image')
-            setModalOpen(false)
-            return
+            setError("Error! Must have an existing image");
+            setModalOpen(false);
+            return;
         }
         try {
             const options = {
-                method: "POST"
-            }
-            const response = await fetch('http://192.168.0.100:8000/variations', options)
-            const data = await response.json()
-            setImages(data)
-            setError(null)
-            setModalOpen(false)
+                method: "POST",
+            };
+            const response = await fetch(
+                "http://192.168.0.100:8000/variations",
+                options
+            );
+            const data = await response.json();
+            setImages(data);
+            setError(null);
+            setModalOpen(false);
         } catch (error) {
-            console.error(error)
+            console.error(error);
         }
-    }
+    };
 
     return (
-        <div className="app">
+        <div className={`app ${darkMode ? "dark-mode" : ""}`}>
+            <button className="surprise toggle-position" onClick={() => setDarkMode(!darkMode)}>
+                {darkMode ? 'Toggle Light Mode' : 'Toggle Dark Mode'}
+            </button>
+
             <h1>GENERATE & MODIFY YOUR OWN IMAGES</h1>
+
             <section className="search-section">
-                <p> Add a description
-                    <span className="surprise" onClick={surpriseMe}>Surprise Me</span>
+                <p>
+                    {" "}
+                    Add a description
+                    <span className="surprise" onClick={surpriseMe}>
+            Surprise Me
+          </span>
                 </p>
                 <div className="input-container">
                     <input
                         value={value}
                         placeholder="A tree in the water"
-                        onChange={(e => setValue(e.target.value))}/>
+                        onChange={(e) => setValue(e.target.value)}
+                    />
                     <button onClick={getImages}>
-                        {generating ? 'Generating...' : 'Generate'}
+                        {generating ? "Generating..." : "Generate"}
                     </button>
-
                 </div>
 
                 <div className="extra-info">
-                <p className="">Or,
-                    <span>
-                        <label htmlFor="files" className="label-file"> upload an image </label>
+                    <p className="">Or,
+                        <span>
+                        <label htmlFor="files" className="label-file">  upload an image </label>
                         <input onChange={uploadImage} id="files" accept="image/png" type="file" hidden/>
                     </span>
-                    to edit.
+                        to edit.
 
-                </p>
+                    </p>
                     {error && <p className="">{error}</p>}
                     {fileError && <p className="">{fileError}</p>}
                 </div>
-
-
 
                 {modalOpen && fileError === null && (
                     <div className="overlay">
@@ -144,17 +169,22 @@ const App = () => {
                             setSelectedImage={setSelectedImage}
                             selectedImage={selectedImage}
                             generateVariations={generateVariations}
+                            darkMode={darkMode}
                         />
                     </div>
                 )}
             </section>
             <section className="image-section">
                 {images?.map((image, _index) => (
-                    <img key={_index} src={image.url} alt={`Generated image of ${value}`}/>
+                    <img
+                        key={_index}
+                        src={image.url}
+                        alt={`Generated image of ${value}`}
+                    />
                 ))}
             </section>
         </div>
     );
-}
+};
 
 export default App;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,31 +1,40 @@
 import {useState, useRef} from "react";
 
-const Modal = ({setModalOpen, setSelectedImage, selectedImage, generateVariations}) => {
-    const [error, setError] = useState(null)
-    const ref = useRef(null)
+const Modal = ({ setModalOpen, setSelectedImage, selectedImage, generateVariations, darkMode }) => {
+    const [error, setError] = useState(null);
+    const ref = useRef(null);
     const closeModal = () => {
-        setModalOpen(false)
-        setSelectedImage(null)
-    }
+        setModalOpen(false);
+        setSelectedImage(null);
+    };
 
     const checkSize = () => {
         if (ref.current.width === 256 && ref.current.height === 256) {
-            generateVariations()
+            generateVariations();
         } else {
-            setError('Error: Choose 256 x 256 image')
+            setError("Error: Choose 256 x 256 image");
         }
-    }
+    };
+
     return (
-        <div className="modal">
-            <div onClick={closeModal} className="label-file">✖</div>
+        <div className={`modal ${darkMode ? "dark-mode" : ""}`}>
+            <div onClick={closeModal} className="label-file">
+                ✖
+            </div>
             <div className="img-container">
-                {selectedImage && <img ref={ref} src={URL.createObjectURL(selectedImage)} alt="uploaded image"/>}
+                {selectedImage && (
+                    <img ref={ref} src={URL.createObjectURL(selectedImage)} alt="uploaded image" />
+                )}
             </div>
             <p>{error || "* Image must be 256 x 256"}</p>
-            {! error && <button onClick={checkSize} className="variation-button">Generate</button>}
-            {error && <button onClick={closeModal}>Close this and try again</button>}
+            {!error && (
+                <button onClick={checkSize} className="variation-button">
+                    Generate
+                </button>
+            )}
+            {error && <button onClick={closeModal} className="variation-button">Close this and try again</button>}
         </div>
-    )
-}
+    );
+};
 
-export default Modal
+export default Modal;
